5a788a1687114a12be208d87acce454a0d21d1f3
Jen Truong <jennifertruong09@gmail.com>
2022-02-19 21:55:24 -0500

    added post notes route to server

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path')
 const fs = require('fs')
 const { v4: uuidv4 } = require('uuid'); 
-const notes = require('./db/db')
+const notes = require('./db/db');
+const { parse } = require('path');
 
 
 const PORT = process.env.PORT || 3001;
@@ -20,21 +21,88 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+//route to get notes
+app.get('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/db/db.json'));
 });
 
+app.get('/api/notes', (req, res) => {
+    fs.readFile('/db/db.json', 'utf8').then((data) => res.json(JSON.parse(data)))
+})
 
-// //route to get notes -- not sure??
-// app.get('/api/notes', (req, res) => {
-//   res.sendFile(path.join(__dirname, '/db/db.json'));
+
+//routes to post notes
+app.post('/api/notes', (req, res) =>{
+    //fs.readFile('/db/db.json', 'utf8').then((notes) => JSON.parse(notes))
+    const { title , text } = req.body;
+
+    const newDb = {
+      title: title, 
+      text: text, 
+      noteId: uuidv4(),
+    }
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          // Convert string into JSON object
+          const parsedDb = JSON.parse(data);
+  
+          // Add a new review
+          parsedDb.push(newDb);
+  
+          // Write updated reviews back to the file
+          fs.writeFile(
+            './db/db.json',
+            JSON.stringify(parsedDb, null, 4),
+            (writeErr) =>
+              writeErr
+                ? console.error(writeErr)
+                : console.info('Successfully updated notes!')
+          );
+        }
+    });
+
+    const response = {
+        status: 'success',
+        body: newDb,
+    };
+      console.log(response);
+      res.status(201).json(response);
+
+})
+
+
+// //route to delete files
+// app.delete('/api/notes/:id', (req,res) => {
+//   const noteId = req.params.noteId
+//   console.log(noteId)
+
+//   fs.readFile('./db/db.json', 'utf8', (err, notes) => {
+//       if(err) {
+//           console.log(err)
+//       } 
+//       notes = JSON.parse(notes)
+
+//       notes = notes.filter((notes) => notes.noteId !== noteId);
+
+//       fs.writeFile('./db/db.json', JSON.stringify(notes), (err, notes) => {
+//           if(err) {
+//               console.log(err);
+//           }
+//           res.json(notes)
+//       })
+  
+ 
+//   });
 // });
 
 
-//route to get notes
-app.get('/api/notes', (req, res) => {
-    res
-})
+//route to all other pages - catch all
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
 
 
 app.listen(PORT, () =>
